4e79e0fab7e37a0a3b1fa457e5d7ab734a7e2797
Richard P Machado <richardmachado2211@gmail.com>
2022-07-19 18:57:24 -0300

    productDetails finalizado

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,34 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    
     // ESCREVA SEUS TESTES ABAIXO:
+
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
+
+// ============================================================================== 
+    // Descobrir se é array https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails('Escova', 'Pasta de dente'))).toBe(true);
+
+// ============================================================================== 
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('Abacaxi', 'Uva').length).toBe(2);
+
+// ============================================================================== 
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('Água', 'Café').keys()).toBe('object');
+
+// ============================================================================== 
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('Alcool gel', 'Máscara')[0]).not.toEqual(productDetails('Alcool gel', 'Máscara')[1]);
+    
+// ============================================================================== 
+    // aprendendo a usar endsWith https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails('Caneta', 'Lápis')[0].details.productId.endsWith('123')).toBe(true);
+    expect(productDetails('Caneta', 'Lápis')[1].details.productId.endsWith('123')).toBe(true);
+
   });
 });
