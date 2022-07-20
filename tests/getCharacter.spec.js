5a9431c6981dd818e91e0be254ae5be13d485b9b
Richard P Machado <richardmachado2211@gmail.com>
2022-07-19 23:11:39 -0300

    getCharacter finalizado

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -33,23 +33,56 @@ Parâmetros:
 Comportamento: 
   - getCharacter('Arya');
 
-Retorno:
-{
+Retorno:*/
+const arya = {
   name: 'Arya Stark',
   class: 'Rogue',
   phrases: [ 'Not today', 'A girl has no name.' ]
+};
+
+const brienne = {
+  name: 'Brienne Tarth',
+  class: 'Knight',
+  phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.']
 }
-*/
+
+const melissandre = {
+  name: 'Melissandre',
+  class: 'Necromancer',
+  phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.']
+}
+
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
-  });
+
+  // ESCREVA SEUS TESTES ABAIXO:
+    test('se a função, quando não recebe nenhum parâmetro, retorna undefined.', () => {
+      expect(getCharacter()).toBeUndefined();
+    });
+
+// ==============================================================================
+    test('se a função retorna o objeto correto para o parâmetro Arya.', () => {
+      expect(getCharacter('Arya')).toStrictEqual(arya); //testar objetos https://jestjs.io/pt-BR/docs/expect#tostrictequalvalue
+    });
+    
+// ==============================================================================    
+    test('se a função retorna o objeto correto para o parâmetro Brienne.', () => {
+      expect(getCharacter('Brienne')).toStrictEqual(brienne);
+    });
+
+// ==============================================================================
+    test('se a função retorna o objeto correto para o parâmetro Melissandre.', () => {
+      expect(getCharacter('Melissandre')).toStrictEqual(melissandre);
+    });
+
+// ==============================================================================    
+    test('se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.', () => {
+      expect(getCharacter('ArYa')).toStrictEqual(arya);
+    });
+
+// ==============================================================================
+    test('se ao passar um nome que não está na tabela, a função retorna undefined.', () => {
+      expect(getCharacter('Richard')).toBeUndefined();
+    });
+
 });
